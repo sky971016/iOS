30d9548daa51e48065fb7ca6d0925b91a010f8cc
Cheung TinLong <sky971016@gmail.com>
2018-06-23 16:33:24 +0800

    Initial Commit

---
+++ final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  final
+//
+//  Created by CheungTin Long on 23/6/2018.
+//  Copyright © 2018年 CheungTin Long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
